51a6734eeb8178cab317bf92b55371a51726c011
jasim-164 <jasimuddincse1703164@gmail.com>
2022-09-21 00:46:39 +0600

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+//ALT+SHIFT+F for decorate code
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/to" element={<Home />}></Route>
+        <Route path="/header" element={<Header/>}></Route>
+      </Routes>
+      
     </div>
   );
 }
